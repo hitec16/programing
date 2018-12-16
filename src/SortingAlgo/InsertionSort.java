558e00269f02024ac239892c2560d1c314021c30
Hitesh Bisht <hitec16@hotmail.com>
2018-12-16 15:54:33 +0530

    added insertion sort

--- src/SortingAlgo/InsertionSort.java
+++ src/SortingAlgo/InsertionSort.java
@@ -1,28 +1,29 @@
 package SortingAlgo;
 
-public class SelectionSort {
-
+public class InsertionSort {
     public static void main(String[] args) {
 
-        Comparable[] ar = {9, 8, 7, 6, 5, 4, 3, 2, 1};
+        Comparable[] ar = {9, 8, 7, 1, 5, 4, 3, 2, 6};
 
-        selectionSort(ar);
+        insertionSort(ar);
 
     }
 
-    public static void selectionSort(Comparable[] a) {
+    public static void insertionSort(Comparable[] a) {
 
         int n = a.length;
 
         for (int i = 0; i < n; i++) {
-            int min = i;
 
-            for (int j = i + 1; j < n; j++) {
-                if (less(a[i], a[j])) {
-                    min = j;
+            for (int j = i; j > 0; j--) {
+                if (less(a[j-1], a[j])) {
+                    exch(a, j, j - 1);
+                }else {
+//                    because we know that their will be no breaks on left side of array when checking for less than condition
+                    break;
                 }
             }
-            exch(a, i, min);
+
         }
 
         for (Comparable item : a) {
@@ -33,7 +34,7 @@ public class SelectionSort {
 
     private static boolean less(Comparable v, Comparable w) {
         if (0 < v.compareTo(w)) {
-//            v=9 , w=8 then true
+            //            v=9 , w=8 then true
             return true;
         } else {
             return false;
@@ -46,5 +47,4 @@ public class SelectionSort {
         a[i] = a[j];
         a[j] = temp;
     }
-
 }
